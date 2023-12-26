c7a660abf8ceb1dde69665e2cc16f2f7c0b56aa0
that_smitty_dude <ThatSmittyDude@outlook.com>
2023-12-26 12:46:06 -0500

    Add files via upload

---
+++ PassingUnderBINv1.1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+#PassingUnderBINv1.1
+
+"""
+Created on Tue Dec 26 11:36:07 2023
+
+@author:    ThatSmittyDude@outlook.com
+            puyinside.com
+            TikTok @ that_smitty_dude
+"""
+
+while True:
+    # Binary
+    print("========================================================")
+    print("| 1024 | 512 | 256 | 128 | 64 | 32 | 16 | 8 | 4 | 2 | 1|")
+    print("========================================================")
+    print("PassingUnderBINv1.1")
+    print(" ")
+    print(" ")
+    print(bin(int(input("enter decimal to convert to BIN: "))))
+    print(" ")
+    print(" ")
+    
+
